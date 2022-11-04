610af83a543d6c6dbd3ef8cc85bc563f2e83b654
user <user@K1625-EP130422>
2022-11-04 17:57:08 +0800

    加入專案檔案。

---
+++ NLogDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using NLog;
+using NLog.Fluent;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Logger log = LogManager.GetCurrentClassLogger();
+        log.Info("這是一個簡單的測試");
+
+    }
+}
